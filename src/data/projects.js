d7553eccac727dc745c8536328f8fe84f33a6854
Thiago Oliveira <thiagodevoliveira@hotmail.com>
2026-05-16 20:27:03 -0300

    Inclusão dos projetos iniciais

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,26 +4,26 @@ const projects = [
         description:
         "Aplicação web educacional para quizzes infantis utilizando React, Node.js e MySQL.",
 
-        github: "https://github.com/SEU_USUARIO",
-        deploy: "https://seu-projeto.vercel.app",
+        github: "https://github.com/th-root/wordgamereact",
+        deploy: "https://wordgamereact.onrender.com/",
     },
 
     {
-        title: "Portal de Chamados RH",
+        title: "Pokedex",
         description:
-            "Portal para gerenciamento de chamados internos utilizando Power Apps.",
+            "Aplicação web para consulta de informações sobre Pokémon utilizando React e a PokéAPI.",
 
-        github: "https://github.com/SEU_USUARIO",
-        deploy: "https://seu-projeto.vercel.app",
+        github: "https://github.com/th-root/Pokedex",
+        deploy: "https://th-root.github.io/Pokedex/",
     },
 
     {
-        title: "Sistema de Recrutamento",
+        title: "Detona RAlph Game",
         description:
             "Sistema web para candidatura e gestão de vagas utilizando HTML, CSS e JavaScript.",
 
-        github: "https://github.com/SEU_USUARIO",
-        deploy: "https://seu-projeto.vercel.app",
+        github: "https://github.com/th-root/detona-ralph-game",
+        deploy: "https://th-root.github.io/detona-ralph-game/",
     },
 ];
 
